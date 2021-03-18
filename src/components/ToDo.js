9763ff4923aaad270c19bb9a63a7758f5bc894d0
Alia Peterson <alia@pnwebs.com>
2021-03-18 16:07:25 -0600

    Create todo component

---
+++ src/components/ToDo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { toggleTodo } from '../actions'
+
+const ToDo = ({ id, text, completed, toggleTodo }) => {
+  return (
+    <li>{text}</li>
+  )
+}
+
+ToDo.propTypes = {
+  todos: PropTypes.array,
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleTodo: (id) => dispatch(toggleTodo(id)),
+})
+
+export default connect(null, mapDispatchToProps)(ToDo)
