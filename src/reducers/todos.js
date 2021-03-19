3fa706dd4572bc9872fc2729c8945cde24d404c8
Alia Peterson <alia@pnwebs.com>
2021-03-19 14:24:58 -0600

    Add reducers for displaying active, completed, and all todos

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -15,6 +15,7 @@ export const todos = (state = initialState, action) => {
           { id: Date.now(), text: action.todo, completed: false }
         ]
       }
+
     case 'TOGGLE_TODO':
       const updatedTodos = state.todos.map(todo => {
         if (todo.id === action.id) {
@@ -23,6 +24,33 @@ export const todos = (state = initialState, action) => {
         return todo
       })
       return {...state, todos: updatedTodos}
+
+    case 'SHOW_ALL':
+      return {
+        ...state,
+        display: 'all'
+      }
+
+    case 'SHOW_ACTIVE':
+      const activeTodos = state.todos.filter(todo => {
+        return !todo.completed
+      })
+      return {
+        ...state,
+        active: activeTodos,
+        display: 'active'
+      }
+
+    case 'SHOW_COMPLETED':
+      const completedTodos = state.todos.filter(todo => {
+        return todo.completed
+      })
+      return {
+        ...state,
+        completed: completedTodos,
+        display: 'completed'
+      }
+
     default:
       return state
   }
