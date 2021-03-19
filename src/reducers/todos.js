ed068548f3526cbb715891f253b28040b19d6b6d
Alia Peterson <alia@pnwebs.com>
2021-03-18 18:52:05 -0600

    Add toggle functionality to reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,7 +1,26 @@
-export const todos = (state = [], action) => {
+const initialState = {
+  todos: [],
+  completed: []
+}
+
+export const todos = (state = initialState, action) => {
   switch(action.type) {
     case 'ADD_TODO':
-      return [...state, { id: Date.now(), text: action.todo, completed: false }]
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: Date.now(), text: action.todo, completed: false }
+        ]
+      }
+    case 'TOGGLE_TODO':
+      const updatedTodos = state.todos.map(todo => {
+        if (todo.id === action.id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {...state, todos: updatedTodos}
     default:
       return state
   }
